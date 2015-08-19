cf5a96d8b52e25def0f0aa24bcb575e4931a5931
uma <uma.palled@manheim.com>
2015-08-19 11:37:57 -0400

    Add content type header to response

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -21,7 +21,7 @@ public class HelloMavenTest extends FunctionalTestCase {
 	public void retrieveFlightsReturnsContentType() throws Exception {
 		MuleEvent event=runFlow("retrieveFlights");
 		String contentType= event.getMessage().getOutboundProperty("Content-Type");
-		assertEquals("application/json",contentType);
+		assertEquals("application/json;charset=UTF-8",contentType);
 	}
 	
 	@Override
